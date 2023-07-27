cfd39242664ec727b7b2328892f3654a9807935c
edvart-ros <94528774+edvart-ros@users.noreply.github.com>
2023-07-27 09:07:51 +0000

    optimizing

--- label_conversion/convert_labels.py
+++ label_conversion/convert_labels.py
@@ -1,8 +1,12 @@
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
+import os
 
-def parse_seg_txt(label_path, height, width):
+import time
+
+
+def parse_seg_txt(label_path, width, height):
     with open(label_path,"r") as f:
         lines = f.readlines()
 
@@ -22,39 +26,53 @@ def parse_seg_txt(label_path, height, width):
     return classes, masks
 
 
-label_path = "original_labels/green_h_image1.txt"
-"""
-
-contour_list = []
-for i in range(len(mask_list)):
-    mask_pad = np.pad(mask_list[i], 1, mode='edge')
-    res = np.clip(mask_pad - cv2.bitwise_and(mask_pad, np.roll(mask_pad, 1, 0)) + \
-                mask_pad - cv2.bitwise_and(mask_pad, np.roll(mask_pad, -1, 0)) + \
-                mask_pad - cv2.bitwise_and(mask_pad, np.roll(mask_pad, 1, 1)) + \
-                mask_pad - cv2.bitwise_and(mask_pad, np.roll(mask_pad, -1, 1)), \
-                a_min = 0, a_max = 1)[1:-1, 1:-1]
+def get_contours_from_masks(mask_list):
+    contour_list = []
+    for i in range(len(mask_list)):
+        mask_pad = np.pad(mask_list[i], 1, mode='edge')
+        res = np.clip(mask_pad - cv2.bitwise_and(mask_pad, np.roll(mask_pad, 1, 0)) + \
+                    mask_pad - cv2.bitwise_and(mask_pad, np.roll(mask_pad, -1, 0)) + \
+                    mask_pad - cv2.bitwise_and(mask_pad, np.roll(mask_pad, 1, 1)) + \
+                    mask_pad - cv2.bitwise_and(mask_pad, np.roll(mask_pad, -1, 1)), \
+                    a_min = 0, a_max = 1)[1:-1, 1:-1]
     contour_list.append(res)
-
-# convert the contours back to the original COCO/Yolo format
-cv2.imwrite("masks_viz/mask0.png", contour_list[0])
-plt.imshow(contour_list[0])
-
-lines = []
-
-for i, contour in enumerate(contour_list):
-    line = []
-    line.append(f"{class_list[i]}")
-    mask_indices = np.where(contour == 1)
-    for i in range(len(mask_indices[0])):
-        x = mask_indices[0][i]
-        y = mask_indices[1][i]
-        line.append(f"{x/width}")
-        line.append(f"{y/width}")
+    return contour_list
     
-    line = ' '.join(line)
-    lines.append(line)
 
-with open('new_labels/green_h_image1.txt', 'w') as f:
-    f.writelines(lines)
-"""
+def convert_contours_to_txt_lines(contour_list, class_list, width, height):
+    lines = []
+    for i, contour in enumerate(contour_list):
+        line = []
+        line.append(f"{class_list[i]}")
+        mask_indices = np.where(contour == 1)
+        for i in range(len(mask_indices[0])):
+            x = mask_indices[0][i]
+            y = mask_indices[1][i]
+            line.append(f"{x/width}")
+            line.append(f"{y/height}")
+        line = ' '.join(line)
+        lines.append(line)
+    return lines
 
+tik = time.time()
+
+width, height = 1920, 1080
+
+label_in_dir = "original_labels/"
+label_out_dir = "new_labels/"
+mask_visualization_dir = "masks_viz/"
+
+# loop through all .txt files in the directory
+for filename in os.listdir(label_in_dir):
+    if filename.endswith(".txt"):
+        label_path = label_in_dir + filename
+        class_list, mask_list = parse_seg_txt(label_path, width, height)
+        contour_list = get_contours_from_masks(mask_list)
+        lines = convert_contours_to_txt_lines(contour_list, class_list, width, height)
+        with open(label_out_dir + filename, "w") as f:
+            f.writelines('\n'.join(lines))
+        # visualize the masks
+        for contour in contour_list:
+            cv2.imwrite(f"{mask_visualization_dir}{os.path.splitext(filename)[0]}.png", contour*255)
+
+print(time.time()-tik)
